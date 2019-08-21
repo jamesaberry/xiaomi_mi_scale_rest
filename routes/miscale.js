9484ad35918300b1fd93cf7d744503ac6ea946c0
jamesaberry <jamesaberry@users.noreply.github.com>
2019-08-21 11:01:35 -0400

    Added udpate patch delete etc

--- routes/miscale.js
+++ routes/miscale.js
@@ -11,6 +11,43 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const scaleEntries = await ScaleEntry.findById(req.params.id);
+        res.json(scaleEntries);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const removed = await ScaleEntry.remove({_id: params.id});
+        res.json(removed);
+    } catch (err) {
+        res.json({ message: err });
+    }
+    
+});
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const updated = await ScaleEntry.updateOne(
+            {_id: params.id}, 
+            {$set: {
+                user: req.body.user,
+                weight: req.body.weight,
+                bmi: req.body.bmi,
+                basal_metabolism: req.body.basal_metabolism,
+                visceral_fat: req.body.visceral_fat,
+                timestamp: req.body.timestamp
+            }});
+        res.json(updated);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
 router.post('/', async (req, res) => {
     const scaleEntry = new ScaleEntry({
         user: req.body.user,
